155080adf5bb4ac98ac6b2a9c6018f1957fb36f2
Neslihand Edes <neslihanedes@gmail.com>
2019-08-20 11:49:41 +0200

    Fix edge case bugs in calculateResponses

--- calculateResponses.m
+++ calculateResponses.m
@@ -1,6 +1,15 @@
 function out = calculateResponses(keyOut)
 
 out.debug = keyOut;
+
+if keyOut.raw(1) == 0
+    out.respPerTrial = 0;
+    out.respPerTrialPerSec = 0;
+    return;
+end
+
+duration = floor(keyOut.stop - keyOut.start);
+
 out.respPerTrial = size(keyOut.raw, 1);
 
 responseTimes = keyOut.rawData(:, 1);
@@ -10,8 +19,8 @@ responsesForSeconds = floor(correctedTimes);
 
 highestValue = responsesForSeconds(end);
 respPerSeconds = zeros(highestValue + 1, 1);
-for n = 1:(highestValue + 1)
-    respPerSeconds(n) = sum(responsesForSeconds == n-1);
+for n = 0:(duration - 1)
+    respPerSeconds(n + 1) = sum(responsesForSeconds == n);
 end
 
 out.respPerTrialPerSec = respPerSeconds;
